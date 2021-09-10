bed9f2a896a750815bff308a449074162197a85f
Marlysson <marlysson5@gmail.com>
2021-09-09 21:02:16 -0300

    Init repository.

---
+++ src/main/java/com/twitch/recoverflow/TwitchRecoverFlowApplication.java
@@ -0,0 +1,13 @@
+package com.twitch.recoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchRecoverFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchRecoverFlowApplication.class, args);
+	}
+
+}
